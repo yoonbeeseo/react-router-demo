75abc7808537812fdac06d0078df6420d93a67ac
yoonbeeseo <yoon.tec.info@gmail.com>
2025-05-27 15:56:59 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
 import { RouterProvider, createBrowserRouter } from "react-router";
 
 export default function App() {
-  const router = createBrowserRouter([
-    {
-      element: (
-        <div>
-          <h1>Home</h1>
-        </div>
-      ),
-      path: "/",
-    },
-    {
-      element: (
-        <div>
-          <h1>Sign-in</h1>
-        </div>
-      ),
-      path: "/sign-in",
-    },
-  ]);
+  const router = createBrowserRouter(
+    [
+      {
+        element: (
+          <div>
+            <h1>Home</h1>
+          </div>
+        ),
+        path: "/",
+      },
+      {
+        element: (
+          <div>
+            <h1>Sign-in</h1>
+          </div>
+        ),
+        path: "/sign-in",
+      },
+      {
+        element: (
+          <div>
+            <h1>Sign-up</h1>
+          </div>
+        ),
+        path: "/sign-up",
+      },
+    ],
+    { basename: "/react-router-demo" }
+  );
   return <RouterProvider router={router} />;
 }
